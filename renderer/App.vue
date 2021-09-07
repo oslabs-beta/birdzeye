8b5222fdf9de0bba9e729149fdf6696b6317e33c
Monchichi <monchichi@Monchichis-Air.lan>
2021-09-07 17:13:25 -0400

    added Terminal Container

--- renderer/App.vue
+++ renderer/App.vue
@@ -1,14 +1,17 @@
 <template>
   <TheTextEditorContainer />
   <h2>hello from vue~</h2>
+  <TheTerminalContainer />
 </template>
 
 <script>
 import TheTextEditorContainer from './textEditor/TheTextEditorContainer.vue';
+import TheTerminalContainer from './terminal/TheTerminalContainer.vue';
 
 export default {
   components: {
     TheTextEditorContainer,
+    TheTerminalContainer,
   }
 }
 </script>
---
+++ renderer/terminal/TheTerminalContainer.vue
@@ -0,0 +1,11 @@
+<template>
+<div>I'm a terminal!</div>    
+</template>
+
+<script>
+export default {
+    data() {
+        
+    }
+}
+</script>
