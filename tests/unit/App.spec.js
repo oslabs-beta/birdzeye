d3876221866d416a5a889da120e9e4c83e921026
Monchichi <monchichi@Monchichis-Air.lan>
2021-10-16 19:27:10 -0400

    first test working

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -2,10 +2,13 @@ import { shallowMount } from '@vue/test-utils';
 import App from '@/App.vue';
 
 describe('App.vue switchToMain', () => {
+  let cmp;
+  beforeEach(() => {
+    cmp = shallowMount(App);
+  })
   it('derenders opening screen', () => {
-    
-    const wrapper = shallowMount(App);
-    App.methods.switchToMain(false);
-    expect(wrapper.displayOpen).toBe(false)
+    // const wrapper = shallowMount(App);
+    cmp.vm.switchToMain(false);
+    expect(cmp.vm.displayOpen).toBe(false)
   })
 })
