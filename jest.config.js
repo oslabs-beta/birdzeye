861f5faa90b5e9b500f656fddc3554e083d90c5d
Natsuki Wada <natsuki@MacBook-Air.lan>
2021-10-16 20:05:12 -0400

    fixed conflict

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,7 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   transform: {
-<<<<<<< HEAD
-    '^.+\\.vue$': 'vue-jest'
-  },
-  transformIgnorePatterns: ['/node_modules/(?!codemirror)']
-=======
     ".*\\.(vue)$": "vue-jest"
   },
   transformIgnorePatterns: ["/node_modules/(?!codemirror)"]
->>>>>>> dev
 }
