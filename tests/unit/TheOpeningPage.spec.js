f2922bd4fb10076bc474485f32401ee1697da67f
Natsuki Wada <natsuki@MacBook-Air.lan>
2021-11-06 09:43:34 -0400

    fixed spacing

--- tests/unit/TheOpeningPage.spec.js
+++ tests/unit/TheOpeningPage.spec.js
@@ -2,23 +2,18 @@ import { shallowMount } from '@vue/test-utils';
 import TheOpeningPage from '@/components/TheOpeningPage.vue';
 
 describe('TheOpeningPage.vue', () => {
-  
   TheOpeningPage.methods.setRoot = jest.fn()
   let wrapper = shallowMount(TheOpeningPage);
 
-
   it('has a button', () => {
-    
     expect(wrapper.html('button')).toBe("<section><button>Open Project</button><img alt=\"birdzeye logo\" src=\"\"></section>")
   })
 
   it('rootDir is empty string by default', () => {
-    
     expect(wrapper.vm.rootDir).toBe('')
   })
 
   it('rootName is empty string by default', () => {
-    
     expect(wrapper.vm.rootName).toBe('')
   })
 })
