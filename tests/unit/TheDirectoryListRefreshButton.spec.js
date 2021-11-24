703e1f7caf9c2a83736c9dde6ff4ca10405b5932
Monchichi <monchichi@Monchichis-Air.lan>
2021-11-23 19:36:19 -0500

    refresh button tests for click

--- tests/unit/TheDirectoryListRefreshButton.spec.js
+++ tests/unit/TheDirectoryListRefreshButton.spec.js
@@ -2,20 +2,18 @@ import { shallowMount } from '@vue/test-utils';
 import TheDirectoryListRefreshButton from '@/components/directory/TheDirectoryListRefreshButton';
 
 describe('TheDirectoryListRefreshButton component', () => {
-  // const mockFunc = jest.fn()
+  
   let wrapper = shallowMount(TheDirectoryListRefreshButton, {
     propsData: {
       rootdir: 'someRootDir'
-    },
-    methods: {
-      refreshDirectoryTree: jest.fn()
     }
   });
   test('that filePath is rootdir from props with a slash', () => {
     expect(wrapper.vm.filePath).toBe('someRootDir/')
   })
   test('that clicking runs the refreshDirectoryTree function', async () => {
+    wrapper.vm.refreshDirectoryTree = jest.fn();
     await wrapper.find('span').trigger('click');
-    expect(wrapper.methods.refreshDirectoryTree.mock.calls.length).toBe(1);
+    expect(wrapper.vm.refreshDirectoryTree).toHaveBeenCalled();
   })
 })
